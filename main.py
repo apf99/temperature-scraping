8ab812a8053fe7f782ebbfc34a4f7940d9ea034c
Andrew Friedman <andrew@cyberdreaming.net>
2024-08-29 15:47:05 -0700

    Added SQLite database for storage

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import sqlite3
 
 URL = 'https://programmer100.pythonanywhere.com/'
+
+# Establish a connection
+connection = sqlite3.connect('temperatures.db')
 
 def scrape(url):
     # scrape the page
@@ -20,30 +24,31 @@ def extract(source):
     return value
 
 
-def store(extracted):
-    with open('data.csv', 'a') as file:
-        file.write(extracted + '\n')
+def store(date, temperature):
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO readings VALUES(?,?)', (date, temperature))
+    connection.commit()
 
 
 def get_data():
-        scraped = scrape(URL)
-        extracted = extract(scraped)
-        now = datetime.now()
-        result = f'{now.strftime('%y-%m-%d-%H-%M-%S')},{extracted}'
-        print(result)
-        store(result)
+    scraped = scrape(URL)
+    temperature = extract(scraped)
+    date = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+    store(date, temperature)
 
 
 st.title('Temperature Data')
-#get the data
 button = st.button('Get Data')
 if button:
     get_data()
 
-df = pd.read_csv('data.csv')
-temperatures = df.temperatures
-dates = df.dates
-labels = {'x': 'Dates', 'y': 'Temperature (C)'}
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM readings')
+results = cursor.fetchall()
+dates = [item[0] for item in results]
+temperatures = [item[1] for item in results]
+
+labels = {'x': 'Dates', 'y':'Temperature (C)'}
 figure = px.line(x=dates, y=temperatures, labels=labels)
 st.plotly_chart(figure)
 
